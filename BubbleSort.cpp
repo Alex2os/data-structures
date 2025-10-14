5afdc7e1a8e5817436e1c10ce7db21ae118b9cdf
Alex <144067034+AlexCats1@users.noreply.github.com>
2025-10-13 19:41:15 -0600

    Add files via upload

---
+++ BubbleSort.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void BubbleSort(int arr[], int tam) {
+
+	for (int i = 0; i < tam - 1 ; i++) {
+
+		bool hayCambio = false;
+
+		for (int j = 0; j < tam - i - 1; j++) {
+
+			if (arr[j] > arr[j + 1]) {
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+				hayCambio = true;
+			}
+		}
+
+		if (!hayCambio) break;
+	}
+
+}
+
+void ImprimirArreglo(int arr[], int tam) { for (int i = 0; i < tam; i++) cout << arr[i] << " "; }
+
+int main() {
+
+	int arreglo[10] = {2, 11 ,4 ,5 ,9 ,10, 76, 8, 4, 29};
+
+	int tam = sizeof(arreglo) / sizeof(arreglo[0]);
+
+	BubbleSort(arreglo, tam);
+
+	ImprimirArreglo(arreglo, tam);
+
+	return 0;
+}
---
+++ SelectionSort.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void SelectionSort(int arr[], int tam) {
+
+	// el hecho de poner el tam - 1 y el i + 1 prevee una iteración extra al final del arreglo.
+	for (int i = 0; i < tam - 1; i++) {
+
+		int indice_minimo = i;
+
+		for (int j = i + 1; j < tam; j++) if (arr[j] < arr[indice_minimo]) indice_minimo = j;
+
+		if (indice_minimo != i) {
+			int temp = arr[i];
+			arr[i] = arr[indice_minimo];
+			arr[indice_minimo] = temp;
+		}
+	}
+}
+
+
+void ImprimirArreglo(int arr[], int tam) { for (int i = 0; i < tam; i++) cout << arr[i] << " "; }
+
+int main() {
+
+	int arreglo[10] = {2, 11 ,4 ,5 ,9 ,10, 76, 8, 4, 29};
+
+	int tam = sizeof(arreglo) / sizeof(arreglo[0]);
+
+	SelectionSort(arreglo, tam);
+
+	ImprimirArreglo(arreglo, tam);
+
+	return 0;
+}
