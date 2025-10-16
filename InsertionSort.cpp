96af144c7a83a41425c281ce53b2aabb1c001484
Alex <144067034+AlexCats1@users.noreply.github.com>
2025-10-15 18:23:04 -0600

    Add files via upload

---
+++ InsertionSort.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+void InsertionSort(int arr[], int tam) {
+
+	for (int i = 1; i < tam; i++) {
+
+		int elemento_actual = arr[i];
+
+		int j = i - 1;
+
+		while (j >= 0 && arr[j] > elemento_actual) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+
+		arr[j + 1] = elemento_actual;
+
+	}
+}
+
+
+void ImprimirArreglo(int arr[], int tam) { for (int i = 0; i < tam; i++) cout << arr[i] << " "; }
+
+int main() {
+
+	int arreglo[10] = {2, 11 ,4 ,5 ,9 ,10, 76, 8, 4, 29};
+
+	int tam = sizeof(arreglo) / sizeof(arreglo[0]);
+
+	InsertionSort(arreglo, tam);
+
+	ImprimirArreglo(arreglo, tam);
+
+	return 0;
+}
