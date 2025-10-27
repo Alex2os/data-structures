988d8a035ed779fd28e4f49b21e0a33ca472fc86
Alex <144067034+AlexCats1@users.noreply.github.com>
2025-10-27 10:51:29 -0600

    added order functions to print the tree's values

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -15,15 +15,17 @@ public:
 };
 
 class BinarySearchTree {
-private:
-	Vertex* root;
 public:
+	Vertex* root;
 
 	BinarySearchTree() {
 		root = nullptr;
 	}
 
 	void Insert(int val);
+	void PreOrder(Vertex *);
+	void PostOrder(Vertex *);
+	void InOrder(Vertex *);
 
 };
 
@@ -63,14 +65,71 @@ void BinarySearchTree::Insert(int val) {
 	}
 }
 
+
+// in the case of the order functions (which are for printing the values of the tree itself) we just have to iterate in different ways, so basically just changing the position of the functions and the cout.
+// basically, when the function hits a vertex that is nullptr, then returns, making the recursivity finish and start printing or doing all the things the function has to do, making this-
+// a way to use recursivity to print the values of the tree.
+void BinarySearchTree::InOrder(Vertex *v) {
+
+	if (v == nullptr) return;
+
+	InOrder(v->next_left);
+
+	cout << v->value << " ";
+
+	InOrder(v->next_right);
+}
+
+void BinarySearchTree::PreOrder(Vertex *v) {
+
+	if (v == nullptr) return;
+
+	cout << v->value << " ";
+
+	PreOrder(v->next_left);
+
+	PreOrder(v->next_right);
+}
+
+void BinarySearchTree::PostOrder(Vertex *v) {
+
+	if (v == nullptr) return;
+
+	PostOrder(v->next_left);
+	PostOrder(v->next_right);
+
+	cout << v->value << " ";
+}
+
 int main() {
 	BinarySearchTree tree = BinarySearchTree();
 
-	tree.Insert(3);
-	tree.Insert(10);
-	tree.Insert(1);
+	// in this case we insert the values of the image of reference to check if the order functions work correctly as intended.
+	tree.Insert(20);
+	tree.Insert(18);
+	tree.Insert(13);
+	tree.Insert(23);
+	tree.Insert(30);
+	tree.Insert(21);
+	tree.Insert(19);
 	tree.Insert(2);
-	tree.Insert(6);
+	tree.Insert(15);
+
+	// print preorder
+	cout << "Printing PreOrder:" << endl;
+	tree.PreOrder(tree.root); // in this case we have to use the tree.root to specify the root of the tree so these functions can start from there. the root attribute of the-
+	                          // BinarySearchTree class is assigned to public for this to be possible, but should check another ways to use send the root to these functions.
+	cout << endl;
+
+	// print inorder
+	cout << "Printing InOrder:" << endl;
+	tree.InOrder(tree.root);
+	cout << endl;
+
+	// print postorder
+	cout << "Printing PostOrder:" << endl;
+	tree.PostOrder(tree.root);
+	cout << endl;
 
 	return 0;
 }
